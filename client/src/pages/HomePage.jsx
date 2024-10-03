df7828774b64b76ae817e906edf2495cf46f55da
abdullah salameh <abdullahsalameh66@gmail.com>
2024-10-03 08:48:23 +0300

    added the base code for featuer I'm not sur if it will stay and made the page responsive

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Typography, useMediaQuery } from '@mui/material'
 import landingPageImage from '../assetes/landingPageImage.png'
 import { useNavigate } from 'react-router-dom'
 
 function HomePage() {
     const navigate = useNavigate()
+    const isMobile = useMediaQuery('(max-width:600px)')
     const features = [
         { title: "Create a CV", caption: "Build and customize your CV with our intuitive CV builder. Highlight your skills and experience to make a lasting impression." },
         { title: "Find Relevant Jobs", caption: "Get personalized job recommendations based on your CV data. Explore opportunities that match your qualifications and interests." },
@@ -17,31 +18,31 @@ function HomePage() {
         <div style={{ width: "100%", height: "100vh", background: "#ffffff", }} >
             <NavBar title="Home" />
 
-            <Box sx={{ display: "flex", width: "100vw", overflowY: "auto", height: "100%", padding: "80px", boxSizing: "border-box" }} >
+            <Box sx={{ display: "flex", width: "100vw", overflowY: "auto", height: "100%", padding: isMobile ? "30px" : "80px", boxSizing: "border-box" }} >
                 <Box sx={{ display: "flex", flex: 1 }}>
 
-                    <Box sx={{ display: "flex", flexDirection: "column",mt:4 }} >
-                        <Typography variant='h2' sx={{ fontWeight: 600 }} >
+                    <Box sx={{ display: "flex", flexDirection: "column", mt: isMobile ? 2 : 4 }} >
+                        <Typography variant='h2' sx={{ fontWeight: 600, textAlign:  "left" }} >
                             Jobs made easy
                         </Typography>
-                        <Typography variant='h2' sx={{ fontWeight: 600 }} >
+                        <Typography variant='h2' sx={{ fontWeight: 600, textAlign: "left" }} >
                             Worldwide
                         </Typography>
 
-                        <Typography variant='body1' sx={{ marginTop: 3, maxWidth: '500px' }}>
-                        Welcome to empolyer, your gateway to finding the perfect job or the ideal candidate. Our platform connects employers and employees with a user-friendly interface designed to streamline the job search and application process.
+                        <Typography variant='body1' sx={{ marginTop: 3, maxWidth: isMobile ? "100%" : "500px", textAlign: "left" }}>
+                            Welcome to empolyer, your gateway to finding the perfect job or the ideal candidate. Our platform connects employers and employees with a user-friendly interface designed to streamline the job search and application process.
                         </Typography>
 
-                        <Box sx={{ display: "flex", marginTop: 3, }} >
+                        <Box sx={{ display: "flex", marginTop: 3, justifyContent: "flex-start" }} >
 
-                            <Button variant="contained" size="large" onClick={()=>navigate('/signup')}>
+                            <Button variant="contained" size="large" onClick={() => navigate('/signup')}>
                                 Sign Up Now
                             </Button>
                         </Box>
 
-                        <Box sx={{ display: "flex", marginTop: 3,fontSize:25,gap:3,color:"#6d7c92",alignItems:"center" }} >
+                        <Box sx={{ display: "flex", marginTop: 3, fontSize: 25, gap: 3, color: "#6d7c92", alignItems: "center", justifyContent: "flex-start" }} >
 
-                            <Typography variant='body2' color="secondary" sx={{color:"#6d7c92"}}>
+                            <Typography variant='body2' color="secondary" sx={{ color: "#6d7c92" }}>
                                 Trusted by cornpanies
                             </Typography>
 
@@ -55,9 +56,43 @@ function HomePage() {
                     </Box>
 
                 </Box>
-                <Box sx={{ display: "flex", flex: 1, justifyContent: "center", alignItems: "flex-start" }}>
-                    <img src={landingPageImage} style={{ width: "80%", height: "auto" }} alt="" />
+                <Box sx={{ display: isMobile ? "none" : "flex", flex: 1, justifyContent: "center", alignItems: "flex-start" }}>
+                    <img src={landingPageImage} style={{ width: isMobile ? "100%" : "80%", height: "auto" }} alt="" />
                 </Box>
+            </Box>
+
+
+
+
+            <Box sx={{ display: "flex", width: "100vw", height: "100%", padding: isMobile ? "20px" : "80px", boxSizing: "border-box" }} >
+
+                <Box sx={{ display: "flex", flexDirection: "column", width: "100%", boxSizing: "border-box" }} >
+                    <Typography variant='h2' sx={{ fontWeight: 600, textAlign: isMobile ? "center" : "left" }} >
+                        Features
+                    </Typography>
+                    <Box sx={{ display: "grid", gridTemplateColumns: { xs: "repeat(1, 1fr)", sm: "repeat(2, 1fr)", lg: "repeat(4, 1fr)" }, gap: 3, width: "100%", mt: 4 }} >
+                        {features.map((feature, index) => (
+                            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-between", border: "1px solid #e5e5e5", borderRadius: 2, p: 2 }} key={index} >
+                                {index === 0 && <i className="fas fa-search" style={{ fontSize: 120, marginTop: 3 }} ></i>}
+                                {index === 1 && <i className="fas fa-user-tie" style={{ fontSize: 120, marginTop: 3 }} ></i>}
+                                {index === 2 && <i className="fas fa-briefcase" style={{ fontSize: 120, marginTop: 3 }} ></i>}
+                                {index === 3 && <i className="fas fa-file-contract" style={{ fontSize: 120, marginTop: 3 }} ></i>}
+                                <Box>
+                                    <Typography variant='h5' sx={{ fontWeight: 600, textAlign: { xs: "center", sm: "left" } }} >
+                                        {feature.title}
+                                    </Typography>
+                                    <Typography variant='body2' sx={{ mt: 1, textAlign: { xs: "center", sm: "left" } }} >
+                                        {feature.caption}
+                                    </Typography>
+                                </Box>
+
+                            </Box>
+                        ))}
+                    </Box>
+                </Box>
+
+
+
             </Box>
 
         </div>
@@ -65,3 +100,4 @@ function HomePage() {
 }
 
 export default HomePage
+
